5cb34570ae1e7e74d5a78099ca84fd67bb1cca68
GhidBase <grvdylans@gmail.com>
2024-11-17 18:38:58 -0700

    Made tic tac toe
    Forgot to commit often, made the whole project
    in one go

---
+++ javascript.js
@@ -0,0 +1,50 @@
+
+let playerScore = 0;
+let computerScore = 0;
+let ties = 0;
+let result = "";
+let playerChoice = "";
+let computerChoice = "";
+
+
+function getComputerChoice() {
+    let randomNumber = Math.random();
+    if (randomNumber < 0.33333) return "rock"
+    else if (randomNumber < 0.66666) return "paper"
+    else return "scissors";
+}
+
+function getHumanChoice() {
+    return prompt("Rock, Paper, or Scissors?").toLowerCase();
+}
+
+function findResult() {
+    if (playerChoice == "rock" && computerChoice == "rock") return "tie";
+    if (playerChoice == "scissors" && computerChoice == "scissors") return "tie";
+    if (playerChoice == "paper" && computerChoice == "paper") return "tie";
+    if (playerChoice == "rock" && computerChoice == "scissors") return "win";
+    if (playerChoice == "rock" && computerChoice == "paper") return "lose";
+    if (playerChoice == "paper" && computerChoice == "scissors") return "lose";
+    if (playerChoice == "paper" && computerChoice == "rock") return "win";
+    if (playerChoice == "scissors" && computerChoice == "paper") return "win";
+    if (playerChoice == "scissors" && computerChoice == "rock") return "lose";
+}
+
+function incrementScore() {
+    if (result == "win") ++playerScore;
+    if (result == "lose") ++computerScore;
+    if (result == "tie") ++ties;
+}
+
+function playRound() {
+    playerChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    result = findResult();
+    incrementScore();
+}
+
+playRound();
+playRound();
+playRound();
+playRound();
+playRound();
